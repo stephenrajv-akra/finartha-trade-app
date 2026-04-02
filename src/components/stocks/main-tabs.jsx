e1e64d9535c7c2f3bc71ad79a9f638dc3d6f079a
stephen <stephenvijay093@gmail.com>
2026-04-02 22:46:49 +0530

    bugs fixes

--- src/components/stocks/main-tabs.jsx
+++ src/components/stocks/main-tabs.jsx
@@ -30,7 +30,7 @@ const MainTabs = ({ gridLayout = 1, onGridChange }) => {
                         className={`px-2 py-2.5 border-r border-[#C8B9D8] rounded-tr-lg text-xs text-black font-medium whitespace-nowrap transition-colors ${
                             activeTab === tab
                                 ? 'bg-white text-black'
-                                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                                : 'text-gray-500 hover:text-gray-700 hover:border-gray-300'
                         }`}
                     >
                         {tab}
