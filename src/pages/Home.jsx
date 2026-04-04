5252531312df5005ce793c79df3a4ea425ed2f39
stephen <stephenvijay093@gmail.com>
2026-04-04 19:34:51 +0530

    added placeholder for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 
+import UnderConstruction from '../components/common/UnderConstruction';
 
 const HomePage = () => {
   return (
-    <h1>Home Page</h1>
-  )
+    <UnderConstruction 
+      title="Welcome to Finartha"
+      description="Your ultimate destination for smarter trading. Get real-time market insights, track global indices, analyze stocks with advanced charting tools, and build your wealth with confidence. The future of trading is here."
+    />
+  ) 
 }
 
 export default HomePage;
