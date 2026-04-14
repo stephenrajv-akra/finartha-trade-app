bb2b911a5a7eec1775304c7563adc74aee4c27b0
Stephenraj <stephenvijay093@gmail.com>
2026-04-14 23:12:11 +0530

    timesales & volAnalysis tab added

--- src/components/stocks/time-analysis.jsx
+++ src/components/stocks/time-analysis.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { timeAndSalesData, volAnalysisData } from '../../utils/placeholder-obj';
 
 const TimeAnalysis = () => {
     const [activeTimeSalesTab, setActiveTimeSalesTab] = useState('timeSales');
@@ -27,23 +28,53 @@ const TimeAnalysis = () => {
             </div>
 
             {/* Tab Content */}
-            <div className="flex-1 flex items-center justify-center overflow-auto">
-                {activeTimeSalesTab === 'timeSales' && (  
-                    <div className="time-analysis flex flex-col items-center justify-center py-12 px-4 gap-2 max-w-[75%] text-center mx-auto"> 
-                        <div className="img-wrapper">
-                            <img src="/icons/search-data.png" alt="search data" />
+            <div className="flex-1 flex items-start justify-center overflow-auto w-full">
+                {activeTimeSalesTab === 'timeSales' && (
+                    <div className="time-analysis w-full p-4">
+                        <div className="overflow-x-auto w-full">
+                            <table className="min-w-full border-separate border-spacing-y-1"> 
+                                <tbody>
+                                    {timeAndSalesData.map((row, idx) => (
+                                        <tr key={idx} className="text-xs font-medium">
+                                            <td className={`${row.symbol === 'BOSS' ? 'text-[#E05C6E]' : 'text-[#2BAF7A]'}`}>{row.time}</td>
+                                            <td className={`${row.symbol === 'BOSS' ? 'text-[#079E53]' : 'text-[#EC4D5C]'}`}>{row.price.toFixed(2)}</td>
+                                            <td className={`${row.symbol === 'BOSS' ? 'text-[#079E53]' : 'text-[#EC4D5C]'}`}>{row.volume}</td>
+                                            <td className={`${row.symbol === 'BOSS' ? 'text-[#079E53]' : 'text-[#EC4D5C]'}`}>{row.symbol}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
-                        <h3 className="text-lg font-normal text-[#7F7F7F]">Time&Sales is not available for this security</h3>
                     </div>
                 )}
 
-                {activeTimeSalesTab === 'volAnalysis' && ( 
-                    <div className="vol-analysis flex flex-col items-center justify-center py-12 px-4 gap-2 max-w-[75%] text-center mx-auto"> 
-                        <div className="img-wrapper">
-                            <img src="/icons/search-data.png" alt="search data" />
+                {activeTimeSalesTab === 'volAnalysis' && (
+                    <div className="vol-analysis w-full p-4">
+                        <div className="overflow-x-hidden w-full">
+                            <table className="min-w-full border-separate border-spacing-y-1">
+                                <thead> 
+                                    <tr className="text-left text-xs text-[#616161] font-semibold">
+                                        <th className="text-[#616161] font-normal">Price</th>
+                                        <th className="text-[#616161] font-normal">Buy</th>
+                                        <th className="text-[#616161] font-normal">Sell</th>
+                                        <th className="text-[#616161] font-normal">Volume</th>
+                                        <th className="text-[#616161] font-normal">Ratio</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {volAnalysisData.map((row, idx) => (
+                                        <tr key={idx} className="text-xs font-medium">
+                                            <td className="pr-6 pl-2 text-[#E05C6E]">{row.price}</td>
+                                            <td className="pr-6 text-[#2BAF7A]">{(row.buy/1000).toFixed(2)}K</td>
+                                            <td className="pr-6 text-[#E05C6E]">{(row.sell/1000).toFixed(2)}K</td>
+                                            <td className="pr-6 text-[#2BAF7A]">{(row.volume/1000).toFixed(2)}K</td>
+                                            <td className="pr-6 text-[#2BAF7A]">{row.ratio.toFixed(2)}%</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
-                        <h3 className="text-lg font-normal text-[#7F7F7F]">Vol Analysis is not available for this security</h3>
-                    </div> 
+                    </div>
                 )}
             </div>
         </>
